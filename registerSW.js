5d8b8458e6dd03ee4dffc4b22a70832e2b2e7e86
Mohammad-Soroush <mysoroush574@gmail.com>
2025-11-12 22:31:42 +0330

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/FlxFy/sw.js', { scope: '/FlxFy/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
